57e29166eb45cd819b55b1f8126fb8e68f65c233
Vinicius Ferreira <viniciusfe66@gmail.com>
2022-02-12 15:37:26 -0400

    corrinfo texto feio

--- admin/header.php
+++ admin/header.php
@@ -16,7 +16,7 @@ if (!isset($_SESSION["id_admin"])) {
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Leiulão duarte | Painel Administrativo</title>
+<title>Leilão Duarte | Painel Administrativo</title>
 
 <link href="css/main.css" rel="stylesheet" type="text/css" />
 <link href='http://fonts.googleapis.com/css?family=Cuprum' rel='stylesheet' type='text/css' />
