44311bde8b43a787050931f657705f53b7b50ac5
Marcos Tokashiki <marcostokashiki@gmail.com>
2022-06-25 17:46:08 -0400

    adição da tela de leilões arrematados

---
+++ leiloesarrematados.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Staatliches&display=swap" rel="stylesheet">
+    <title>Leilões arrematados</title>
+</head>
+<body>
+    <header></header>
+<div class="body">
+    <div class="tudo">
+    <div>
+    <p class="text-start">Lista de leilões arrematados</p>
+    </div>
+    <div class="conteiner">
+    <div class="item-conteiner conteiner 1">
+    <div class="text-conteiner">
+        <p class="text-content paragrafo">Iniciou dia dd/mm/aaaa 00:00:00</p>
+        <p class="text-content paragrafo">Terminou dia dd/mm/aaaa 00:00:00</p>
+    </div>
+        <p class="text-content item">Item 1</p>
+        <div class="img-content"><img src="https://cdn-icons-png.flaticon.com/512/1942/1942023.png" alt=""></div>
+        <div class="valor-item">R$ 00,00</div>
+    </div>
+    <div class="item-conteiner conteiner 2">
+    <div class="text-conteiner">
+        <p class="text-content paragrafo">Iniciou dia dd/mm/aaaa 00:00:00</p>
+        <p class="text-content paragrafo">Terminou dia dd/mm/aaaa 00:00:00</p>
+    </div>
+        <p class="text-content item">Item 2</p>
+        <div class="img-content"><img src="https://cdn-icons-png.flaticon.com/512/1942/1942023.png" alt=""></div>
+        <div class="valor-item">R$ 00,00</div>
+    </div>
+    <div class="item-conteiner conteiner 3">
+<div class="text-conteiner">        
+    <p class="text-content paragrafo">Iniciou dia dd/mm/aaaa 00:00:00</p>
+    <p class="text-content paragrafo">Terminou dia dd/mm/aaaa 00:00:00</p>
+</div>
+    <p class="text-content item">Item 3</p>
+    <div class="img-content"><img src="https://cdn-icons-png.flaticon.com/512/1942/1942023.png" alt=""></div>
+    <div class="valor-item">R$ 00,00</div>
+    </div>
+</div>
+</div>
+</div>
+
+</body>
+</html>
